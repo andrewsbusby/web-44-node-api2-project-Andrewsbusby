bcfd58e6fbc6169905c0b4ce9c6fd204f9602202
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-03 12:17:47 -0600

    server is set up

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 // implement your server here
 const express = require('express');
+const postRouter = require('./posts/posts-router');
 const server = express();
 
 server.unsubscribe(express.json());
 // require your posts router and connect it here
+server.use('/api/posts/posts-router.js', postRouter);
 
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Checkout these posts</h1>`)
+})
 
 module.exports = server;
