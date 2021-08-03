e298b047f2101aba003821e5961fce03745d160e
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-03 12:54:21 -0600

    delete, get comments, and post are set up

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -36,4 +36,61 @@ router.get('/:id', (req, res) => {
         })
 })
 
+// CREATE A NEW POST
+router.post('/', (req, res) =>{
+    Post.insert(req.body)
+        .then(newPost => {
+            if (newPost) {
+                res.status(201).json(newPost)
+            } else {
+                res.status(400).json({
+                    message: "Please provide title and contents for the post"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "There was an error while saving the post to the database"
+            })
+        })
+})
+
+// DELETE POST
+router.delete('/:id', (req, res) => {
+    Post.remove(req.params.id)
+        .then(removed => {
+            if (!removed) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else {
+                res.status(200).json(removed)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post could not be removed"
+            })
+        })
+})
+
+// GET POST BY ID AND COMMENTS
+router.get('/:id/comments', (req, res) => {
+    Post.findPostComments(req.params.id)
+        .then(comments => {
+            if (!comments) {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            } else {
+                res.status(200).json(comments)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The comments information could not be retrieved"
+            })
+        })
+})
+
 module.exports = router;
