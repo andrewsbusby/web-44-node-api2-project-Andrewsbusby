572ed946f86e05159b58b354bcea7d24be0dcec1
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-03 12:04:08 -0600

    setting up server and index

--- api/server.js
+++ api/server.js
@@ -1,2 +1,9 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
+server.unsubscribe(express.json());
 // require your posts router and connect it here
+
+
+module.exports = server;
