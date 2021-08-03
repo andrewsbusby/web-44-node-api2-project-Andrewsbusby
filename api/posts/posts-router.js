7238a4750d3564a1310e24bbdbae0f9080ec4172
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-03 12:28:51 -0600

    findbyid is working and passing test

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,6 +4,7 @@ const Post = require('./posts-model');
 
 const router = express.Router()
 
+// FIND ALL POSTS
 router.get('/', async (req, res) => {
     try {
         const posts = await Post.find(req.query)
@@ -14,6 +15,25 @@ router.get('/', async (req, res) => {
             message: "The posts information could not be retrieved"
         })
     }
+})
+
+// FIND POST BY ID
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be retrieved"
+            })
+        })
 })
 
 module.exports = router;
